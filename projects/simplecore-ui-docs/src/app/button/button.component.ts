ce28c3e9b4cf8390237b44bcead835f84fd01439
Justin V <simplecore>
2020-11-23 21:39:28 -0800

    Feature: Add color and size theming to button

--- projects/simplecore-ui-docs/src/app/button/button.component.ts
+++ projects/simplecore-ui-docs/src/app/button/button.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  template: '<button coreButton>Test</button>',
+  template: `
+    <button size="sm" coreButton>Small</button>
+    <button size="md" coreButton>Medium</button>
+    <button size="lg" coreButton>Large</button>
+
+    <button themeColor="secondary" size="md" coreButton>Small</button>
+  `,
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
