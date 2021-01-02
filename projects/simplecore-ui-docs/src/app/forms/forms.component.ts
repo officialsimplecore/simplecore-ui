40ed9e1ba2cbe9b1de3b7c4fc7e980fdb66916e3
Justin V <simplecore>
2021-01-02 11:09:59 -0800

    Build select documentation

---
+++ projects/simplecore-ui-docs/src/app/forms/forms.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.scss']
+})
+export class FormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  logForm(value: any) {
+    console.log(value);
+  }
+}
