33658f7c9d79bb2896d76decd4cd3dd3214731c0
Justin V <officialsimplecore>
2021-01-02 11:06:28 -0800

    Feature: Set up card component

---
+++ projects/simplecore-ui/src/lib/card/card.module.ts
@@ -0,0 +1,20 @@
+﻿/**
+ * @license
+ * Copyright 2021 SimpleCore All Rights Reserved.
+ *
+ * Use of this source code is governed private licensing for
+ * internal SimpleCore projects
+ */
+
+import {NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CoreCard} from "./card";
+
+@NgModule({
+  declarations: [CoreCard],
+  imports: [
+    CommonModule
+  ],
+  exports:[CoreCard]
+})
+export class CoreInputModule { }
---
+++ projects/simplecore-ui/src/lib/card/card.ts
@@ -0,0 +1,28 @@
+﻿/**
+ * @license
+ * Copyright 2021 SimpleCore All Rights Reserved.
+ *
+ * Use of this source code is governed private licensing for
+ * internal SimpleCore projects
+ */
+
+import {ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'input[coreCard]',
+  exportAs: 'coreCard',
+  template: '<ng-content></ng-content>',
+  styleUrls: ['card.scss'],
+  host: {
+    'class': 'core-card',
+  },
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CoreCard implements OnInit {
+  constructor(private element: ElementRef) {
+  }
+  ngOnInit(): void {
+    // this.element.nativeElement.classList.add("core-input");
+  }
+}
