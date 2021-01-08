c47332d0927569b07f465006c1812f39a66afa39
Justin V <officialsimplecore>
2021-01-08 09:05:19 -0800

    Update public api

--- projects/simplecore-ui/src/public-api.ts
+++ projects/simplecore-ui/src/public-api.ts
@@ -13,3 +13,7 @@ export * from './lib/button/index';
 export * from './lib/input/index';
 
 export * from './lib/select/index';
+
+export * from './lib/card/index';
+
+export * from './lib/_directives/pressable/index';
