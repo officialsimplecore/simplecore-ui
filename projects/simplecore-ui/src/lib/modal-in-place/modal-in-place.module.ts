a040f1c7557ac8a884c4ded17b2604b69e75e7c4
Justin Vassantachart <justin@simplecore.org>
2022-09-05 17:14:41 -0700

    Correct errors with deprecated SCSS

--- projects/simplecore-ui/src/lib/modal-in-place/modal-in-place.module.ts
+++ projects/simplecore-ui/src/lib/modal-in-place/modal-in-place.module.ts
@@ -3,11 +3,13 @@ import { OverlayModule } from '@angular/cdk/overlay';
 
 import { ModalInPlace } from './modal-in-place';
 import { ModalToggleDirective } from './modal-toggle.directive';
+import {A11yModule} from "@angular/cdk/a11y";
 
 @NgModule({
   declarations: [ModalInPlace, ModalToggleDirective],
   imports: [
-    OverlayModule
+    OverlayModule,
+    A11yModule
   ],
   exports: [ModalInPlace, ModalToggleDirective]
 })
